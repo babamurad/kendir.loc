62b0dde1f58640a2a7568448ee66a66eade2d734
guvanchkabulov <94168128+guvanchkabulov@users.noreply.github.com>
2024-02-19 11:18:28 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', function () {
-//     return view('welcome');
+//    hi from kabulov
 // });
 
 Route::get('/', HomeComponent::class)->name('home');
