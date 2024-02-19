bfc28f09a6ff8a245bb2e13064e690c7c5049548
babamurad <babamurad2010@yandex.ru>
2024-02-19 11:34:49 +0500

    Chanche logo to White

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -314,43 +314,11 @@
                     <div class="site-header__logo">
                         <a href="/">
                             <!-- logo -->
-                            <img src="{{ asset('images/kendir/logo.png') }}" alt="Kendir Logo" style="width: 14rem;">
+                            <img src="{{ asset('images/kendir/kendir-logo-white.png') }}" alt="Kendir Logo" style="width: 14rem;">
                             <!-- logo / end -->
                         </a>
                     </div>
-                    <div class="site-header__search">
-                        <div class="search search--location--header ">
-                            <div class="search__body">
-                                <form class="search__form" action="">
-                                    <select class="search__categories" aria-label="Category">
-                                        <option value="all">All Categories</option>
-                                        <option>Instruments</option>
-                                        <option>&nbsp;&nbsp;&nbsp;&nbsp;Power Tools</option>
-                                        <option>&nbsp;&nbsp;&nbsp;&nbsp;Hand Tools</option>
-                                        <option>&nbsp;&nbsp;&nbsp;&nbsp;Machine Tools</option>
-                                        <option>&nbsp;&nbsp;&nbsp;&nbsp;Power Machinery</option>
-                                        <option>&nbsp;&nbsp;&nbsp;&nbsp;Measurement</option>
-                                        <option>&nbsp;&nbsp;&nbsp;&nbsp;Clothes and PPE</option>
-                                        <option>Electronics</option>
-                                        <option>Computers</option>
-                                        <option>Automotive</option>
-                                        <option>Furniture & Appliances</option>
-                                        <option>Music & Books</option>
-                                        <option>Health & Beauty</option>
-                                    </select>
-                                    <input class="search__input" name="search" placeholder="Search over 10,000 products"
-                                        aria-label="Site search" type="text" autocomplete="off">
-                                    <button class="search__button search__button--type--submit" type="submit">
-                                        <svg width="20px" height="20px">
-                                            <use xlink:href="images/sprite.svg#search-20"></use>
-                                        </svg>
-                                    </button>
-                                    <div class="search__border"></div>
-                                </form>
-                                <div class="search__suggestions suggestions suggestions--location--header"></div>
-                            </div>
-                        </div>
-                    </div>
+                    <livewire:header-search-component />
                     <div class="site-header__phone">
                         <div class="site-header__phone-title">Customer Service</div>
                         <div class="site-header__phone-number">(800) 060-0730</div>
